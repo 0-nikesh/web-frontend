79e2a7eebdd9155a425184dc8f2267eb55fa2fc7
nikesh <bhandarinikesh93@gmail.com>
2025-02-14 00:39:49 +0545

    F12-04- draw lines in map

---
+++ src/components/RoutingMachine.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";  // Add this import for useEffect
+import { useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+
+export const RoutingMachine = ({ start, end }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+
+    const routingControl = L.Routing.control({
+      waypoints: [L.latLng(start[0], start[1]), L.latLng(end[0], end[1])],
+      routeWhileDragging: true,
+    }).addTo(map);
+
+    return () => map.removeControl(routingControl);
+  }, [map, start, end]);
+
+  return null;
+};
